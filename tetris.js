d1b95c4b733098b38a96398675ae5a890245ea1c
uuganbaatar <uuganbaatar@uuganbaatars-MacBook-Air.local>
2021-08-16 13:32:27 -0700

    v2

--- tetris.js
+++ tetris.js
@@ -3,6 +3,8 @@ const context = canvas.getContext("2d");
 
 context.scale(20, 20);
 
+
+//arenasweep function ni hamgiin door bga mur ni neg buten bolhdn arilgj onoo bolgdg function bga.
 function arenaSweep() {
   let rowCount = 1;
   outer: for (let y = arena.length - 1; y > 0; --y) {
@@ -21,9 +23,11 @@ function arenaSweep() {
     }
 }
 
+// collide function ni matrix dotor bga durs deer hoosn zai baigaag shalgaj deern davharldg function bga. 
+
 function collide(arena, player) {
-  const m = player.matrix;
-  const o = player.pos;
+  const m = player.matrix;  // m ni dursee hyanah gej shine matrix uusgej bga.
+  const o = player.pos; // o ni bolhoor dursee buulgah gej ashiglana. 
   for (let y = 0; y < m.length; ++y) {
     for (let x = 0; x < m[y].length; ++x) 
    
@@ -36,6 +40,7 @@ function collide(arena, player) {
   return false;
 }
 
+  //  createMatrix function bol shine matrix uusgej width height avnaa. 
 function createMatrix(w, h) {
   const matrix = [];
   while (h--) {
@@ -43,6 +48,8 @@ function createMatrix(w, h) {
   }
   return matrix;
 }
+
+// createPiece function ni matrix deer durs buteej bga.
 
 function createPiece(type) {
   if (type === "I") {
@@ -90,6 +97,7 @@ function createPiece(type) {
   }
 }
 
+// drawMatrix function ni dursiig buulgaj zai baigaa esehiig shalgadag function bga. uurur helvel cordinant aar shalgj bga.
 function drawMatrix(matrix, offset) {
   matrix.forEach((row, y) => {
     
@@ -98,11 +106,12 @@ function drawMatrix(matrix, offset) {
           
         context.fillStyle = colors[value];
         context.fillRect(x + offset.x, y + offset.y, 1, 1);
+      
       }
     });
   });
 }
-
+// draw function bol talbaig zurj bna. hex code oor  #000 bol har ungiig ilerhiilj bna. 
 function draw() {
   context.fillStyle = "#000";
   context.fillRect(0, 0, canvas.width, canvas.height);
@@ -111,15 +120,20 @@ function draw() {
   drawMatrix(player.matrix, player.pos);
 }
 //Endees ehelnee
+// mergre function ni bol x , y cordinant iig update hiij bna umnuh baigaa dursniha update hiij bna. Y ni 0 tei tentsuu baival game over bolno.
 function merge(arena, player) {
   player.matrix.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value !== 0) {
         arena[y + player.pos.y][x + player.pos.x] = value;
+      
       }
     });
   });
 }
+
+// rotate function ni matrix, dir gesen 2 value avnaa. rotate function dotor loop yvna. y ni 0 ees ehelj matrix lenght hurtel yvnaa neg looped 1 eer nemne. 
+// ehnii loop yvmagts dotrn dahiad neg loop yvna. ter loopd bol yg adilhn gehde x cordinantaar yvna. 
 
 function rotate(matrix, dir) {
   for (let y = 0; y < matrix.length; ++y) {
@@ -134,7 +148,7 @@ function rotate(matrix, dir) {
     matrix.reverse();
   }
 }
-
+// playerDrop function ni  
 function playerDrop() {
   player.pos.y++;
   if (collide(arena, player)) {
